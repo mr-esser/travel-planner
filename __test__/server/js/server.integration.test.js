b7b2ae276445c3f53e95528cef731c764b267a69
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-09-26 18:59:12 +0200

    chore: Extend itests for geodata service

--- __test__/server/js/server.integration.test.js
+++ __test__/server/js/server.integration.test.js
@@ -17,3 +17,59 @@ test('GET / should yield 200', () => {
       .get('/')
       .expect(200);
 });
+
+describe('GET on /geodata', () => {
+  test(`should yield one geo record given valid params given`,
+      () => {
+        return/* ! */ request(server)
+            .get(`/geodata?city=Berlin&country=DE`)
+            .expect('Content-Type', /json/)
+            .expect(200).then((res) => {
+              expect(res.body.totalResultsCount).toBeGreaterThan(0);
+              expect(res.body.geonames.length).toBe(1);
+              const firstRecord = res.body.geonames[0];
+              expect(firstRecord.name).toBe('Berlin');
+              expect(firstRecord.countryCode).toBe('DE');
+              expect(firstRecord.lng).toBe('13.41053');
+              expect(firstRecord.lat).toBe('52.52437');
+            });
+      },
+  );
+  test(`should fail with HTTP 400 given invalid param 'city'`,
+      () => {
+        return/* ! */ request(server)
+            .get('/geodata?city=')
+            .expect(400)
+            .expect('Content-Type', /text\/html/)
+            .then((res) => {
+              expect(res.text)
+                  .toMatch(/Param &#39;city&#39; must not be empty<br>/);
+            });
+      });
+  test(`should fail with HTTP 400 given invalid param 'country'`,
+      () => {
+        return/* ! */ request(server)
+            .get('/geodata?city=Berlin&country=DUCK')
+            .expect(400)
+            .expect('Content-Type', /text\/html/)
+            .then((res) => {
+              expect(res.text)
+                  .toMatch(
+                      /Param &#39;country&#39; must be a two-letter-code<br>/,
+                  );
+            });
+      });
+  test(`should fail with HTTP 400 given no query`,
+      () => {
+        return/* ! */ request(server)
+            .get('/geodata')
+            .expect(400)
+            .expect('Content-Type', /text\/html/)
+            .then((res) => {
+              expect(res.text)
+                  .toMatch(/Param &#39;city&#39; must not be empty<br>/);
+            });
+      });
+
+  /* 500 is hard to provoke */
+});
