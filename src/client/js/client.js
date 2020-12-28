b6ce2eed334c3201245b1871b5084bd29674cc31
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-12-28 22:39:20 +0100

    feat: Wire up client UI and back-end
    Trip info can be entered. Data gets collected from the web APIs and visualized on the page.

--- src/client/js/client.js
+++ src/client/js/client.js
@@ -1,3 +1,8 @@
+import {fetchGeoData} from './fetchGeoData';
+import {fetchWeatherData} from './fetchWeatherData';
+import {fetchImageData} from './fetchImageData';
+import {buildTripData} from './aggregateData';
+// import {json} from 'body-parser';
 /* Global Variables */
 
 // Journal server info
@@ -19,19 +24,33 @@ const getInputText = function(selector) {
 
 /* Update the most recent entry in the UI */
 const updateUI = function({
-  date = '',
-  location = '',
-  temperature = '',
-  content = '',
+  city = '',
+  country = '',
+  departureDate = '',
+  returnDate = '',
+  duration = '',
+  forecasts = [],
+  image = '',
 }) {
   const container = document.querySelector('#entryHolder');
   // Trying to avoid several consecutive reflows/repaints here.
   // Does not seem to be worth the effort, though.
   container.style = 'display: none;';
-  container.querySelector('#date').innerHTML = date;
-  container.querySelector('#location').innerHTML = location;
-  container.querySelector('#temp').innerHTML = temperature;
-  container.querySelector('#content').innerHTML = content;
+  container.querySelector('#depart-date').innerHTML = departureDate;
+  container.querySelector('#return-date').innerHTML = returnDate;
+  container.querySelector('#duration').innerHTML =
+    duration.toString() + ' days';
+  container.querySelector('#location').innerHTML = `${city}, ${country}`;
+  container.querySelector('#pic').innerHTML = `<img
+  src="${image.url}"
+  alt="Image showing landmark of ${city}, ${country}">`;
+  container.querySelector('#weather').innerHTML = `<div> 
+  <div>${forecasts[0].datetime}</div>
+  <img src="${forecasts[0].icon}">
+  <div>${forecasts[0].description}</div>
+  <div>${forecasts[0].min_temp}</div>
+  <div>${forecasts[0].max_temp}</div>
+  </div>`;
   container.style = '';
 };
 
@@ -59,43 +78,60 @@ const getFromServer = async function(url = '') {
 
 /* MAIN function called by event listener on 'Generate' button */
 const handleGenerate = async function handleGenerate(event) {
-  const buildTripData = async function() {
-    const tripData = buildTripData();
+  const buildDisplayData = async function() {
+    let displayData = {};
     try {
-      const weatherUrl = getServiceUrl('weather');
-      console.debug(weatherUrl);
-      const zipData = {zipAndCountryCode: getInputText('#zip')};
-      console.debug(JSON.stringify(zipData));
-      const weatherData = await postToServer(weatherUrl, zipData);
-      // Temperature unit is hard-coded. See constant UNITS.
-      tripData.temperature = `${weatherData.main.temp} °C`;
-      tripData.location = `${weatherData.name}, ${weatherData.sys.country}`;
+      const city = getInputText('#city');
+      console.debug('city: ' + city);
+      const country = getInputText('#country');
+      console.debug('country: ' + country);
+      // Assuming that inputs have been validated by the UI
+      const depart = getInputText('#depart');
+      console.debug('departure: ' + depart);
+      const ret = getInputText('#return');
+      console.debug('return: ' + ret);
+
+      const geoUrl = getServiceUrl('geodata');
+      console.debug(geoUrl?.toString());
+
+      // TODO: Run things in parallel!
+      const geoData = await fetchGeoData(city, country);
+      const lng = geoData.geonames[0].lng;
+      const lat = geoData.geonames[0].lat;
+      console.debug(`fetch forecast for: ${lat} and ${lng}`);
+      const weatherData = await fetchWeatherData(lat, lng);
+      const imageData = await fetchImageData(city);
+      displayData =
+        buildTripData(city, country, depart, ret, weatherData, imageData);
+      // TODO: Temperature unit is hard-coded?
     } catch (error) {
       // Data is incomplete, but it can still be posted and displayed.
       // So, log and then go on.
       console.info(error);
     }
-    return tripData;
+    return displayData;
   };
 
-  const synchronizeWithServer = async function(journalData) {
-    console.debug(JSON.stringify(journalData));
-    const route = 'all';
-    await postToServer(getServiceUrl(route), journalData);
-    return getFromServer(getServiceUrl(route));
+  const synchronizeWithServer = async function(payload) {
+    const route = 'trips';
+    const received = await postToServer(getServiceUrl(route), payload);
+    if (false) getFromServer();
+    return received;
   };
 
   try {
-    const journalData = await buildTripData();
-    const serverData = await synchronizeWithServer(journalData);
-    updateUI(serverData);
+    const tripData = await buildDisplayData();
+    const serverRecord = await synchronizeWithServer(tripData);
+    console.debug('Complete data: ' + JSON.stringify(serverRecord));
+    updateUI(serverRecord);
   } catch (networkError) {
     // Probably unrecoverable networking error ,i.e.: fetch rejected and threw
     console.error(networkError);
     // Very lazy!! Should rather use a dedicated <div>
     // to display the error and hide the rest.
     updateUI({
-      date: 'Ooops. Looks like the service is down. Please, try again later.',
+      departureDate:
+        'Ooops. Looks like the service is down. Please, try again later.',
     });
   }
 };
@@ -109,7 +145,8 @@ window.addEventListener('DOMContentLoaded', () => {
   generateButton.addEventListener('click', handleGenerate);
   // Try to load data from the server on startup.
   // There may already be some available.
-  getFromServer(getServiceUrl('all'))
-      .then((data) => updateUI(data))
-      .catch((error) => console.error(error));
+  // TODO: Re-enable!
+  // getFromServer(getServiceUrl('all'))
+  //     .then((data) => updateUI(data))
+  //     .catch((error) => console.error(error));
 });
