6b37e73fc5cab443fdc75d2ce3d23d9c856f68d5
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2021-01-10 21:16:18 +0100

    refactor: Update TODO

--- src/server/index.js
+++ src/server/index.js
@@ -5,5 +5,5 @@ server.listen(PORT, function() {
   console.log(`Server listening on port ${PORT}`);
   console.log(`Server working dir is '${__dirname}'`);
   // Note(!): env is prepared by the server module.
-  // TODO: List the relevant API keys here.
+  // TODO: Check for relevant API keys here. Don't echo!
 });
